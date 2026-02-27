d91552cf40d9e9c4ede8fe9fed44a286c05f0cce
Maria Abi Nakhoul <mabi-nak@c1r3s4.42beirut.com>
2026-02-27 20:17:53 +0200

    created the files

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,20 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <exception>
+#include <string>
+
+class BitcoinExchange
+{
+	private:
+		
+
+	public:
+
+}
+
+#endif
