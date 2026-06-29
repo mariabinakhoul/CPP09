d347d67ea475b85f5c127a28cc247e63d9ef27df
Maria Abi Nakhoul <mabi-nak@c1r2s3.42beirut.com>
2026-06-29 17:50:00 +0300

    files created and.hpp done

---
+++ ex01/RPN.hpp
@@ -0,0 +1,26 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <stack>
+#include <fstream>
+
+class RPN
+{
+	private:
+		std::stack<double> _stack;
+		
+		bool _isOperator(const std::string	&token) const;
+		bool _isNumber(const std::string &token) const;
+		bool _applyOperator(const std::string &op);
+
+	public:
+		RPN();
+		RPN(const RPN& source);
+		RPN& operator=(const RPN& source);
+		~RPN();
+
+		double evaluate(const std::string &expression);
+};
+
+#endif
